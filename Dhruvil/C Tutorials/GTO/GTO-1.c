6221fc7090f40ae85167cf904860a06288ce372e
dhruvil-84 <150236262+dhruvil-84@users.noreply.github.com>
2023-11-08 15:26:34 +0530

    Delete Dhruvil directory

--- Dhruvil/C Tutorials/GTO/GTO-1.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("/nn/nnn/n");
-    printf("\nnn\nnn/n\nn");
-    return 0;
-}
--- Dhruvil/C Tutorials/GTO/GTO-6.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("The Output is : \n");
-    printf("%d", 2*(2 == ((sizeof((float)4))/2)));
-    printf("\n%f", (0.5*(sizeof((int)6.000000))));
-    return 0;
-}
